501ac75a522656246221c726814cf249c0a597a7
Aecop <joonruiz08@gmail.com>
2023-04-15 12:50:00 -0700

    profile page created in front end

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,10 +1,14 @@
 import Navbar from "./Navbar";
 import SmallSidebar from "./SmallSidebar";
 import BigSidebar from "./BigSidebar";
+import Alert from './Alert';
+import FormRow from './FormRow'
 
 
 export {
     Navbar,
     SmallSidebar,
-    BigSidebar
+    BigSidebar,
+    Alert,
+    FormRow
 }
